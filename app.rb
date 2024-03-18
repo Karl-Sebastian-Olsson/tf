aea962b346d839c7757561e540ce18d2177932d5
Karl-Sebastian-Olsson <karl.olsson@elev.ga.ntig.se>
2024-03-18 14:52:12 +0100

    e

--- app.rb
+++ app.rb
@@ -7,12 +7,28 @@ get('/') do
     slim(:index)
 end 
 
+#          BYT NAMN PÅ IMAGES(folder) TILL ALBUM, INDEX TILL START OCH IMAGES.slim TILL INDEX
+
 get('/images') do 
     db = SQLite3::Database.new("model/db/store.db")
     db.results_as_hash = true
     result = db.execute("SELECT * FROM Images")
-    p result
-    slim(:"images/images",locals:{images:result})
+    slim(:"images/images", locals:{images:result})
+end 
+
+get('/images/:id') do 
+    id = params[:id].to_i
+    db = SQLite3::Database.new("model/db/store.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM Images WHERE Iid = ?", id).first
+    slim(:"images/show", locals:{result:result})
+end 
+
+post('/images/:id/delete') do 
+    id = params[:id].to_i
+    db = SQLite3::Database.new("model/db/store.db")
+    db.execute("DELETE FROM Images WHERE Iid = ?", id)
+    redirect('/images')
 end 
 
 get('/images/new') do 
