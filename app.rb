b6dd21178bac2def65314fb1f3e1404f84234332
Karl-Sebastian-Olsson <karl.olsson@elev.ga.ntig.se>
2024-03-18 16:47:55 +0100

    fixed "new.slim"

--- app.rb
+++ app.rb
@@ -4,19 +4,32 @@ require 'slim'
 require 'sqlite3'
 
 get('/') do 
-    slim(:index)
+    slim(:start)
 end 
 
-#          BYT NAMN PÅ IMAGES(folder) TILL ALBUM, INDEX TILL START OCH IMAGES.slim TILL INDEX
+# BYT NAMN PÅ IMAGES(folder) TILL ALBUM, INDEX TILL START OCH IMAGES.slim TILL INDEX
+# LÄGG TILL 404 IMAGE OM BILD EJ LADDAR 
 
-get('/images') do 
+get('/gallery') do 
     db = SQLite3::Database.new("model/db/store.db")
     db.results_as_hash = true
     result = db.execute("SELECT * FROM Images")
-    slim(:"images/images", locals:{images:result})
+    slim(:"images/index", locals:{result:result})
 end 
 
-get('/images/:id') do 
+get('/gallery/new') do 
+    slim(:"images/new")
+end 
+
+post('/gallery/new') do 
+    title = params[:title]
+    url = params[:url]
+    db = SQLite3::Database.new("model/db/store.db")
+    db.execute("INSERT INTO Images (Title, Url) VALUES (?, ?)", title, url)
+    redirect('/gallery')
+end 
+
+get('/gallery/:id') do 
     id = params[:id].to_i
     db = SQLite3::Database.new("model/db/store.db")
     db.results_as_hash = true
@@ -24,21 +37,9 @@ get('/images/:id') do
     slim(:"images/show", locals:{result:result})
 end 
 
-post('/images/:id/delete') do 
+post('/gallery/:id/delete') do 
     id = params[:id].to_i
     db = SQLite3::Database.new("model/db/store.db")
     db.execute("DELETE FROM Images WHERE Iid = ?", id)
-    redirect('/images')
-end 
-
-get('/images/new') do 
-    slim(:"images/new")
-end 
-
-post('/images/new') do 
-    title = params[:title]
-    url = params[:url]
-    db = SQLite3::Database.new("model/db/store.db")
-    db.execute("INSERT INTO Images (Title, Url) VALUES (?, ?)", title, url)
-    redirect('/images')
+    redirect('/gallery')
 end 
