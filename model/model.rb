2185c9904d68489e3bd109384ec20a0729a86528
Karl-Sebastian-Olsson <karl.olsson@elev.ga.ntig.se>
2024-05-09 20:22:30 +0200

    -||-

--- model/model.rb
+++ model/model.rb
@@ -92,38 +92,3 @@ module Modules
         slim(:"users/index", locals:{result:result, role:role, like:like, liked:liked})
     end
 end
-
-
-# module Modules
-#     def db_define()
-#         db = SQLite3::Database.new("model/db/store.db")
-#         db.results_as_hash = true
-#         return db
-#     end
-
-#     def authorize(db, uid)
-#         if uid != 0
-#             return db.execute("SELECT Role FROM Users WHERE Uid = ?", uid).first["Role"]
-#         else 
-#             return "Guest"
-#         end
-#     end 
-
-#     def show_all_images(session_id)
-#         db = db_define()
-#         result = db.execute("SELECT * FROM Images") 
-#         role = authorize(db, session_id.to_i)
-#         like = db.execute("SELECT * FROM User_image_junction WHERE Uid=?", session[:id].to_i)
-#         slim(:"images/index", locals:{result:result, role:role, like:like})
-#     end
-
-#     def create_new_image(title, desc, url)
-#         db = db_define()
-#         db.execute("INSERT INTO Images (Title, Desc, Url, Uid) VALUES (?, ?, ?, ?)", title, desc, url, session[:id].to_i)
-#         redirect('/gallery')
-#     end
-
-
-
-
-# end
