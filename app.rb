b16921ff14717d9856bbe6b236647cdb67704c96
Karl-Sebastian-Olsson <karl.olsson@elev.ga.ntig.se>
2024-05-13 13:12:06 +0200

    yardoc klar

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require 'sinatra/reloader'
 require 'slim'
 require 'sqlite3'
 require 'bcrypt'
+require 'sinatra/flash'
 require_relative './model/model.rb'
 
 include Modules
@@ -69,54 +70,106 @@ post('/gallery/:id/delete') do
     delete_image(params[:id].to_i)
 end 
 
-
+# Visar redigeringsformuläret för en specifik bild
+#
+# @param id [Integer] ID för den specifika bilden
+# @param session_id [String] Användarens session-id
+# @see Modules#get_image_details
+# @return [String] Renderar redigeringsformuläret för en specifik bild
 get('/gallery/:id/edit') do 
     slim(:"/images/edit", locals: {result: get_image_details(params[:id].to_i, session[:id])})
 end 
 
-
+# Uppdaterar information om en specifik bild
+#
+# @param id [Integer] ID för den specifika bilden
+# @param title [String] Uppdaterad titel på bilden
+# @param desc [String] Uppdaterad beskrivning av bilden
+# @param url [String] Uppdaterad URL till bildfilen
+# @see Modules#update_image
+# @return [Redirect] Omdirigerar användaren till gallerivyn
 post('/gallery/:id/update') do 
     update_image(params[:id].to_i, params[:title], params[:desc], params[:url])
 end 
 
-
+# Visar registreringsformuläret för nya användare
+#
+# @return [string] Renderar registreringsformuläret
 get('/user/new') do 
     slim(:"register")
 end 
 
-
+# Hanterar registrering av en ny användare
+#
+# @param user [String] Användarnamnet
+# @param email [String] E-postadressen
+# @param pswd [String] Lösenordet
+# @param pswd_confirm [String] Bekräftelse av lösenordet
+# @see Modules#manage_user_registration
+# @return [Redirect/String] Omdirigerar till startsidan eller visar felmeddelande
 post('/user/new') do
     manage_user_registration(params[:user], params[:email], params[:pswd], params[:pswd_confirm])
 end 
 
-
+# Visar inloggningssidan
+#
+# @return [String] Renderar inloggningssidan
 get('/login') do
     slim(:"login")
 end 
 
-
+# Hanterar inloggning för en användare
+#
+# @param user [String] Användarnamnet
+# @param pswd [String] Lösenordet
+# @param session [Hash] Session-hashen som används för att spara användar-id
+# @see Modules#user_login
+# @return [Redirect/String] Omdirigerar till startsidan eller visar felmeddelande
 post('/login') do 
     user_id = user_login(params[:user], params[:pswd], session)
 end 
 
+# Hanterar utloggning för en användare
+#
+# @return [Redirect] Rensar sessionen och omdirigerar till startsidan
 get('/logout') do
     session.clear
     redirect('/')
 end
 
+# Visar galleriet för den inloggade användaren
+#
+# @param session_id [String] Användarens session-id
+# @see Modules#user_gallery
+# @return [String] Renderar användarens personliga galleri
 get('/user') do
     user_gallery(session[:id])
 end 
+
+# Gör att man kan gilla en specifik bild
+#
+# @param id [Integer] ID för den specifika bilden
+# @param session_id [String] Användarens session-id
+# @param action [String] Aktionen att utföra ('like')
+# @see Modules#manage_like
+# @return [Redirect] Omdirigerar användaren tillbaka till bilden --------------> Ändra till redirec till tidigare sida 
 
 post('/gallery/:id/like') do
     manage_like(session[:id].to_i, params[:id].to_i, 'like')
 end 
 
+# Gör att man kan ogilla en specifik bild
+#
+# @param id [Integer] ID för den specifika bilden
+# @param session_id [String] Användarens session-id
+# @param action [String] Aktionen att utföra ('unlike')
+# @see Modules#manage_like
+# @return [Redirect] Omdirigerar användaren tillbaka till bilden
 post('/gallery/:id/unlike') do
     manage_like(session[:id].to_i, params[:id].to_i, 'unlike')
 end 
 
 
+
+# MELLANRUM MELLAN DOC OCH GET
 # KOLLA OM UNSERNAME REDAN FINNS ELLER INTE -- UTVECKLA DET SOM FINNS 
-# LÄGG TIL LATT MAN KAN UPPLOADA BILDER ISTÄLLET FÖR LÄNKAR på bilder 
-# YARDOC FUCKER
--- model/model.rb
+++ model/model.rb
@@ -1,9 +1,22 @@
+#Övergripande modul för metoderna
+
 module Modules
+    # Definierar och returnerar en databasanslutning
+    #
+    # @return [SQLite3::Database] En instans av SQLite3-databasen, inställd för att returnera hashar
+
     def db_define()
         db = SQLite3::Database.new("model/db/store.db")
         db.results_as_hash = true
+        db.execute('PRAGMA foreign_keys = ON')
         return db
     end
+
+    # Kontrollerar användarens roll i systemet
+    #
+    # @param db [SQLite3::Database] Databasanslutningen
+    # @param uid [Integer] Användar-ID
+    # @return [String] Användarens roll i systemet eller "Guest" om användaren inte är identifierad
 
     def authorize(db, uid)
         if uid != 0
@@ -13,6 +26,11 @@ module Modules
         end
     end 
 
+    # Hämtar alla bilder och deras associerade information från databasen
+    #
+    # @param session_id [Integer] Användarens session-ID
+    # @return [String] Renderar en vy som listar alla bilder med tillhörande detaljer och användarroll
+
     def show_all_images(session_id)
         db = db_define()
         result = db.execute("SELECT * FROM Images") 
@@ -21,11 +39,24 @@ module Modules
         slim(:"images/index", locals:{result:result, role:role, like:like})
     end
 
+    # Skapar en ny bildpost i databasen
+    #
+    # @param title [String] Titeln på bilden
+    # @param desc [String] Beskrivning av bilden
+    # @param url [String] URL till bildens lagringsplats
+    # @param session_id [Integer] Användarens session-ID
+    # @return [Redirect] Omdirigerar användaren till galleriet efter att bilden skapats
+
     def create_new_image(title, desc, url, session_id)
         db = db_define()
         db.execute("INSERT INTO Images (Title, Desc, Url, Uid) VALUES (?, ?, ?, ?)", title, desc, url, session_id)
         redirect('/gallery')
     end
+
+    # Tar bort en bild från databasen baserat på dess ID
+    #
+    # @param image_id [Integer] ID för bilden som ska tas bort
+    # @return [Redirect] Omdirigerar användaren till galleriet efter att bilden tagits bort
 
     def delete_image(image_id)
         db = db_define()
@@ -33,11 +64,25 @@ module Modules
         redirect('/gallery')
     end
 
+    # Uppdaterar en befintlig bild i databasen
+    #
+    # @param image_id [Integer] ID för bilden som ska uppdateras
+    # @param title [String] Ny titel för bilden
+    # @param desc [String] Ny beskrivning av bilden
+    # @param url [String] Ny URL för bildens lagringsplats
+    # @return [Redirect] Omdirigerar användaren till galleriet efter att uppdateringen genomförts
+
     def update_image(image_id, title, desc, url)
         db = db_define()
         db.execute("UPDATE Images SET Title=?, Desc=?, Url=? WHERE Iid = ?", title, desc, url, image_id)
         redirect('/gallery')
     end
+
+    # Hämtar detaljerad information om en specifik bild
+    #
+    # @param image_id [Integer] ID för bilden som detaljerna avser
+    # @param session_id [Integer] Användarens session-ID
+    # @return [String] Renderar en vy med detaljer om den specifika bilden
 
     def get_image_details(image_id, session_id)
         db = db_define()
@@ -46,6 +91,14 @@ module Modules
         like = db.execute("SELECT * FROM User_image_junction WHERE Uid=?", session_id.to_i)
         slim(:"images/show", locals:{result:result, role:role, like:like})
     end
+
+    # Hanterar registreringsprocessen för nya användare
+    #
+    # @param username [String] Användarnamnet
+    # @param email [String] Användarens e-postadress
+    # @param pswd [String] Användarens lösenord
+    # @param pswd_confirm [String] Bekräftelse av användarens lösenord
+    # @return [Redirect/String] Omdirigerar till startsidan om registreringen lyckas, annars visas ett felmeddelande
 
     def manage_user_registration(username, email, pswd, pswd_confirm)
         db = db_define()
@@ -62,6 +115,13 @@ module Modules
         end
     end
 
+    # Hanterar inloggningsprocessen för användare
+    #
+    # @param username [String] Användarnamnet
+    # @param pswd [String] Användarens lösenord
+    # @param session [Hash] Session-hash som håller användarens ID om inloggningen lyckas
+    # @return [Redirect/String] Omdirigerar till startsidan om inloggningen lyckas, annars visas ett felmeddelande
+
     def user_login(username, pswd, session)
         db = db_define()
         result = db.execute("SELECT * FROM Users WHERE Name = ?", username).first
@@ -73,6 +133,13 @@ module Modules
         end
     end
 
+    # Hanterar "like"- eller "unlike"-åtgärder för en specifik bild
+    #
+    # @param session_id [Integer] Användarens session-ID
+    # @param image_id [Integer] Bildens ID som ska "likes" eller "unlikes"
+    # @param action [String] Åtgärden att utföra ('like' eller 'unlike')
+    # @return [Redirect] Omdirigerar tillbaka till den specifika bildsidan
+
     def manage_like(session_id, image_id, action)
         db = db_define()
         if action == 'like'
@@ -82,6 +149,11 @@ module Modules
         end
         redirect("/gallery/#{image_id}")
     end
+
+    # Hämtar alla bilder som tillhör en specifik användare
+    #
+    # @param session_id [Integer] Användarens session-ID
+    # @return [String] Renderar en vy med alla bilder som användaren har lagt till
 
     def user_gallery(session_id)
         db = db_define()
